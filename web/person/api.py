638d9bc23e51d9e1bcd37193938131119bdd98d7
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-07-08 14:45:13 +0300

    Added person app

---
+++ web/person/api.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from flask import (abort, Blueprint, request, render_template)
+
+from web.person.models import Person
+
+from web.helpers import api_helper, header_helper
+
+
+logger = logging.getLogger(__name__)
+
+blueprint = Blueprint("api", __name__, url_prefix='/terminal')
+
+
+@blueprint.route("/firm_id/<int:firm_id>/persons/", methods=['GET'])
+@header_helper.xml_headers
+@api_helper.login_required
+def getUsers(firm_id):
+
+    search = {}
+    for key in Person.SEARCH_KEY:
+        search_value = request.args.get(key)
+        if not search_value:
+            continue
+        search[key] = search_value
+
+    limit = api_helper.get_request_count(request, Person.PER_PAGE)
+    offset = api_helper.get_request_offset(request)
+    query = Person.query.filter_by(firm_id=firm_id)
+    if search:
+        for key, value in search.iteritems():
+            query = query.filter(getattr(Person, key).like('%' + value + '%'))
+
+    persons = query.limit(limit).offset(offset).all()
+    if not persons:
+        abort(404)
+
+    return render_template("person/person.xml", persons=persons)
+
+
+@blueprint.route("/firm_id/<int:firm_id>/persons/", methods=['POST'])
+@header_helper.xml_headers
+@api_helper.login_required
+def addUser(firm_id):
+    search = {}
+    for key in Person.MANDATORY_PARAMETERS:
+        search_value = request.form.get(key)
+        if not search_value:
+            continue
+        search[key] = search_value
+
+    if len(search) != len(Person.MANDATORY_PARAMETERS):
+        if not request.form.get(key):
+            abort(405)
+
+    person = Person.query.filter_by(firm_id=firm_id).\
+        filter((Person.hard_id == search['hard_id']) |
+               (Person.tabel_id == search['tabel_id'])).first()
+
+    if person:
+        force = request.form.get('force')
+        if not force:
+            abort(400)
+
+        result = Person.delete(person)
+        if not result:
+            abort(500)
+
+    person = Person()
+    person.firm_id = firm_id
+    for key, value in search.iteritems():
+        setattr(person, key, value)
+
+    for key in Person.OPTIONAL_PARAMETERS:
+        value = request.form.get(key)
+        if not value:
+            continue
+
+        setattr(person, key, value)
+
+    if not person.payment_id:
+        person.payment_id = person.hard_id
+
+    if not person.save():
+        abort(500)
+
+    return render_template("person/person.xml", persons=(person,))
+
+
+@blueprint.route("/firm_id/<int:firm_id>/persons/", methods=['DELETE'])
+@header_helper.xml_headers
+@api_helper.login_required
+def delUser(firm_id):
+    hard_id = request.args.get('hard_id')
+    if not hard_id:
+        abort(405)
+
+    person = Person.query.filter_by(firm_id=firm_id).\
+        filter_by(hard_id=hard_id).first()
+    if not person:
+        abort(404)
+
+    result = Person.delete(person)
+    if not result:
+        abort(500)
+
+    return render_template("person/success.xml")
---
+++ web/person/models.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+import logging
+
+from web.database import Model, SurrogatePK, db, ReferenceCol, relationship
+from web.helpers import date_helper
+
+logger = logging.getLogger(__name__)
+
+
+class Person(SurrogatePK, Model):
+
+    __tablename__ = 'person'
+
+    PER_PAGE = 50
+
+    STATUS_VALID = 1
+    STATUS_BANNED = 0
+
+    TYPE_TIMEOUT = 0
+    TYPE_WALLET = 1
+
+    SEARCH_KEY = ('name', 'tabel_id', 'card', 'hard_id')
+
+    MANDATORY_PARAMETERS = ('name', 'tabel_id', 'hard_id')
+
+    OPTIONAL_PARAMETERS = ('payment_id', 'card')
+
+    name = db.Column(db.Text, nullable=False)
+    tabel_id = db.Column(db.String(150))
+    birthday = db.Column(db.Date())
+    firm_id = ReferenceCol('firm', nullable=False)
+    firm = relationship('Firm', backref='person')
+    card = db.Column(db.String(8))
+    payment_id = db.Column(db.String(20), nullable=False, index=True)
+    hard_id = db.Column(db.String(128), nullable=False)
+    creation_date = db.Column(db.DateTime, nullable=False)
+    status = db.Column(db.Integer, nullable=False, index=True)
+    wallet_status = db.Column(db.Integer, nullable=False, index=True)
+    type = db.Column(db.Integer, nullable=False, index=True)
+
+    def __init__(self):
+        self.status = self.STATUS_VALID
+        self.wallet_status = self.STATUS_VALID
+        self.type = self.TYPE_TIMEOUT
+        self.creation_date = date_helper.get_current_date()
+        self.name = u'Пользователь'
+
+    @staticmethod
+    def delete(person):
+        try:
+            db.session.query(PersonEvent).\
+                filter(PersonEvent.person_id == person.id).delete()
+            db.session.query(PersonWallet).\
+                filter(PersonWallet.person_id == person.id).delete()
+            db.session.delete(person)
+            db.session.commit()
+        except Exception, e:
+            db.session.rollback()
+            logger.error(e)
+            return False
+
+        return True
+
+
+class PersonEvent(SurrogatePK, Model):
+
+    __tablename__ = 'person_event'
+
+    STATUS_ACTIVE = 1
+    STATUS_BANNED = 0
+
+    person_id = ReferenceCol('person', nullable=False)
+    person = relationship('Person', backref='person_event')
+    term_id = db.Column(db.Integer, nullable=False, index=True)
+    event_id = ReferenceCol('event', nullable=False)
+    event = relationship('Event', backref='person_event')
+    firm_id = ReferenceCol('firm', nullable=False)
+    firm = relationship('Firm', backref='person_event')
+    timeout = db.Column(db.Integer, nullable=False)
+    status = db.Column(db.Integer, nullable=False)
+
+
+class PersonWallet(SurrogatePK, Model):
+
+    __tablename__ = 'corp_wallet'
+
+    person_id = ReferenceCol('person', nullable=False)
+    person = relationship('Person', backref='corp_wallet')
+    creation_date = db.Column(db.DateTime, nullable=False)
+    balance = db.Column(db.Integer, nullable=False)
+    limit = db.Column(db.Integer, nullable=False)
+    interval = db.Column(db.Integer, nullable=False, index=True)
+    status = db.Column(db.Integer, nullable=False, index=True)

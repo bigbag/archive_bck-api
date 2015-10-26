2f94426e7d06e1c9187c28908f7de8f236401594
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-10-26 15:02:46 +0300

    Added delete card function

--- web/person/api.py
+++ web/person/api.py
@@ -14,25 +14,34 @@ logger = logging.getLogger(__name__)
 blueprint = Blueprint("api", __name__, url_prefix='/bck')
 
 
+def get_parameter():
+    try:
+        search = json.loads(request.stream.read())
+    except ValueError:
+        return
+    else:
+        parameter = search.get('Parameter')
+        if parameter:
+            return parameter
+
+    logger.debug('API: Not found Parameter in request parameters')
+    return
+
+
 @blueprint.route("/GetCard/", methods=['POST'])
 @logging_helper.debug_request
 @header_helper.json_headers
 @api_helper.login_required
-def get_card():
+def get_persons():
     limit = api_helper.get_request_count(request, Person.PER_PAGE)
     offset = api_helper.get_request_offset(request)
-    query = Person.query.filter_by(firm_id=g.user.firm)
+    query = Person.query.filter(Person.firm_id.in_(g.firms))
 
-    try:
-        search = json.loads(request.stream.read())
-    except ValueError:
-        pass
-    else:
-        parameter = search.get('Parameter')
-        if parameter:
-            cards_id = parameter.get('CardId')
-            if cards_id:
-                query = query.filter(Person.id.in_(cards_id))
+    parameter = get_parameter()
+    if parameter:
+        cards_id = parameter.get('CardId')
+        if cards_id:
+            query = query.filter(Person.id.in_(cards_id))
 
     persons = query.limit(limit).offset(offset).all()
     if not persons:
@@ -43,6 +52,40 @@ def get_card():
 
     result = {
         'Data': data,
+        'State': {
+            'IsSuccess': True,
+            'Code': '0000'
+        }
+    }
+    return jsonify(result)
+
+
+@blueprint.route("/DelCard/", methods=['POST'])
+@logging_helper.debug_request
+@header_helper.json_headers
+@api_helper.login_required
+def del_person():
+
+    parameter = get_parameter()
+    if not parameter:
+        abort(405)
+
+    card_id = parameter.get('CardId')
+    if not card_id:
+        logger.debug('API: Not found CardId in request parameters')
+        abort(405)
+
+    person = Person.query.filter(Person.firm_id.in_(g.firms)).\
+        filter_by(id=card_id).first()
+    if not person:
+        logger.debug('API: Not found person')
+        abort(404)
+
+    result = Person.delete(person)
+    if not result:
+        abort(500)
+
+    result = {
         'State': {
             'IsSuccess': True,
             'Code': '0000'

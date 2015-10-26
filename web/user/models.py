d4709352b797e7ce2257631c58a3dcd9e2240092
Lyashkov Pavel <bilbo.kem@gmail.com>
2015-10-26 10:54:20 +0300

    Add scheme column in user model

--- web/user/models.py
+++ web/user/models.py
@@ -10,9 +10,13 @@ class User(SurrogatePK, Model):
     STATUS_ACTIVE = 1
     STATUS_BANNED = 0
 
+    SCHEME_EGGER = 'egger'
+    SCHEME_BCK = 'bck'
+
     email = db.Column(db.String(128), nullable=False, index=True)
     key = db.Column(db.String(150))
     secret = db.Column(db.String(150))
     app = db.Column(db.Text, nullable=False)
     firm = db.Column(db.Text, nullable=False)
+    scheme = db.Column(db.String(64), nullable=False)
     status = db.Column(db.Integer, index=True)
